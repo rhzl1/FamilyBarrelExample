99f0f882853346a437ad89cd2cf589db4ad9ff0c
wf66 <408963215@qq.com>
2018-11-08 14:51:06 +0800

    修改包名

--- src/main/java/com/example/sqlProviderTool/HomeSqlProvider.java
+++ src/main/java/com/example/sqlProviderTool/HomeSqlProvider.java
@@ -1,8 +1,11 @@
-package com.example.SqlProviderTool;
+package com.example.sqlProviderTool;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.jdbc.SQL;
 
+/**
+ * 对应SQL
+ */
 public class HomeSqlProvider {
 
     public String selectAll(@Param("suName")String suName,@Param("phone")String phone){
@@ -11,10 +14,10 @@ public class HomeSqlProvider {
             {
                 SELECT("suName,phone");
                 FROM("sys_user");
-                if (suName != "" && suName != null){
+                if (suName.equals("") && suName.isEmpty()){
                     WHERE("suName=#{suName}");
                 }
-                if(phone != "" && phone != null){
+                if(phone.equals("") && phone.isEmpty()){
                     WHERE("phone=#{phone}");
                 }
             }
